6909317339c1e4d717b328bea462e66753453507
gilmerperez <gilmer2002@outlook.com>
2025-01-31 19:19:38 -0500

    Finished schema.sql

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -1,2 +1,35 @@
+-- Create Database
 DROP DATABASE IF EXISTS employee_tracker_db;
 CREATE DATABASE employee_tracker_db;
+
+-- Select Database for use
+\c employee_tracker_db;
+
+-- Drop Tables so we don't see errors when running project multiple times
+DROP TABLE IF EXISTS department;
+DROP TABLE IF EXISTS role;
+DROP TABLE IF EXISTS employee;
+
+-- Create department table
+CREATE TABLE department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
+);
+
+-- Create role table
+CREATE TABLE role (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER NOT NULL
+);
+
+-- Create employee table
+CREATE TABLE employee (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER NOT NULL,
+    manager_id INTEGER,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
